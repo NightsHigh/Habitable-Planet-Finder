9a9ddd0830cce2fa23c89f67d17104d2ab8a1528
NightsHigh <soren913@gmail.com>
2025-01-29 12:22:30 +0100

    Refractored the code to be more UI friendly in the planet divs

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -16,8 +16,8 @@ function displayPlanets(planets) {
             <div class="planet-details">
                 <h2 class="planet-title">${planet.kepler_name}</h2>
                 <div class="planet-meta">
-                    <span class="planet-temperature"><strong>Temperature:</strong> ${planet.koi_insol}</span>
-                    <span class="planet-size"><strong>Size:</strong> ${planet.koi_prad} * Earth's radius</span>
+                    <span class="planet-temperature"><h4><strong>Light:</strong></h4> ${planet.kepler_name} gets ${planet.koi_insol} times the amount of the sunlight Earth gets.</span>
+                    <span class="planet-size"><h4><strong>Size:</strong></h4> ${planet.koi_prad *  6378} KM in radius</span>
                 </div>
             </div>
         `;
